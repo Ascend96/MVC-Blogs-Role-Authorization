429b3f9e8ca06ca7b528172e8b7d1d3aaddc2251
Ascend96 <mmartens28@yahoo.com>
2021-02-23 13:12:41 -0600

    Add Post to Blog
    - Update BloggingContext class - implement AddPost method
    - Add Required attribute to post title
    - Add method to Home controller to return view
    - Add html form to add new post
    - Add method to Home controller to handle post request

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,6 +37,25 @@ namespace Blogs.Controllers
             _bloggingContext.DeleteBlog(_bloggingContext.Blogs.FirstOrDefault(b => b.BlogId == id));
             return RedirectToAction("Index");
         }
+                 public IActionResult AddPost(int id)
+        {
+            ViewBag.BlogId = id;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddPost(int id, Post post)
+        {
+            post.BlogId = id;
+            if (ModelState.IsValid)
+            {
+                _bloggingContext.AddPost(post);
+                return RedirectToAction("BlogDetail", new { id = id });
+            }
+            @ViewBag.BlogId = id;
+            return View();
+        }
         public IActionResult BlogDetail(int id) => View(new PostViewModel
         {
             blog = _bloggingContext.Blogs.FirstOrDefault(b => b.BlogId == id),
